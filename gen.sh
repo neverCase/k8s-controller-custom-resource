24ab0dab7998fa8a36911bf0a7783283891f5ed8
neverCase <1024769485@qq.com>
2020-02-27 18:32:10 +0800

    redis doc

--- gen.sh
+++ gen.sh
@@ -5,9 +5,9 @@ export GOPATH="/Users/SAM/go"
 # 代码生成的工作目录，也就是我们的项目路径
 ROOT_PACKAGE="github.com/nevercase/k8s-controller-custom-resource"
 # API Group
-CUSTOM_RESOURCE_NAME="samplecrd"
+CUSTOM_RESOURCE_NAME="redisoperator"
 # API Version
 CUSTOM_RESOURCE_VERSION="v1"
 
 # 执行代码自动生成，其中pkg/client是生成目标目录，pkg/apis是类型定义目录
-/Users/SAM/go/src/k8s.io/code-generator/generate-groups.sh all "$ROOT_PACKAGE/pkg/client" "$ROOT_PACKAGE/pkg/apis" "$CUSTOM_RESOURCE_NAME:$CUSTOM_RESOURCE_VERSION"
+/Users/SAM/go/src/k8s.io/code-generator/generate-groups.sh a "$ROOT_PACKAGE/pkg/client" "$ROOT_PACKAGE/pkg/apis" "$CUSTOM_RESOURCE_NAME:$CUSTOM_RESOURCE_VERSION"
--- pkg/apis/redisoperator/v1/doc.go
+++ pkg/apis/redisoperator/v1/doc.go
@@ -1,1 +1,4 @@
+// +k8s:deepcopy-gen=package
+
+// +groupName=redisoperator.nevercase.io
 package v1
